b6e8d31eb8b41070e777998bdb14b7fa8da0ca38
rajendrachy <chyrajendra32@gmail.com>
2025-06-30 00:33:27 +0530

    up to 10 min complete part 28

---
+++ PART28/RandomJokGen/app.js
@@ -0,0 +1,32 @@
+// ----------Random Joke Generator App------------------
+
+import https from 'https';
+import chalk from 'chalk';
+
+const getJoke = () => {
+  const url = 'https://official-joke-api.appspot.com/random_joke';
+
+  https.get(url, (res) => {
+    let data = "";
+
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      try {
+        const joke = JSON.parse(data);
+
+        console.log(chalk.green('Here is a random joke:'));
+        console.log(chalk.red(joke.setup));
+        console.log(chalk.blue.bgRed.bold(joke.punchline));
+      } catch (error) {
+        console.error(chalk.red('Failed to parse joke:'), error.message);
+      }
+    });
+  }).on('error', (err) => {
+    console.error(chalk.red('Request failed:'), err.message);
+  });
+};
+
+getJoke();
