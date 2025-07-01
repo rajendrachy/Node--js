b7545750a42e9d3bdadc8184fe12cc673776aff4
rajendrachy <chyrajendra32@gmail.com>
2025-07-01 11:43:45 +0530

    lab1 complete

---
+++ CLASSNOTE/LAB1/app.js
@@ -0,0 +1,38 @@
+// // app.js (Synchronous version)
+// const fs = require('fs');
+
+// // Read file
+// const data = fs.readFileSync('secE.txt', 'utf8');
+// console.log('Before:', data);
+
+// // Write new content
+// fs.writeFileSync('secE.txt', data + '\nAdded with sync method.');
+// console.log('Synchronous write done.');
+
+
+
+
+
+
+
+
+
+
+//-----------Asynchronous-------------------
+const fs = require('fs');
+
+fs.readFile('secE.txt', 'utf8', (err, data) => {
+  if (!err) {
+    console.log('Before:', data);
+
+    const updated = `${data}\nAdded with async method.`;
+
+    fs.writeFile('secE.txt', updated, 'utf8', (err) => {
+      if (!err) console.log('Write done.');
+      else console.error('Write error:', err);
+    });
+  } else {
+    console.error('Read error:', err);
+  }
+});
+
