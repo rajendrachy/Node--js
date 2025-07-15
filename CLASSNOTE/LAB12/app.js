10b8f3b302d7cd937726f1129831d9acf4607a18
rajendrachy <chyrajendra32@gmail.com>
2025-07-15 22:38:50 +0530

    routes complete lab12

---
+++ CLASSNOTE/LAB12/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const baseRoute = require("./routes/baseRoute");
+
+// Middleware to parse form data (optional, good for POST handling)
+app.use(express.urlencoded({ extended: true }));
+
+// Use base routes
+app.use("/", baseRoute);
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
+
+
