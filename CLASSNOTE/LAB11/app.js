9efc86a2553173af1aa642333b99b47885f1aa3b
rajendrachy <chyrajendra32@gmail.com>
2025-07-15 22:39:45 +0530

    test assignment of express js

---
+++ CLASSNOTE/LAB11/app.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/about', (req, res) => {
+      res.sendFile(path.join(__dirname , './public/about.html'));
+})
+
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/contact.html'));
+})
+
+app.get('/services', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/services.html'));
+})
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Server start...");
+})
+
